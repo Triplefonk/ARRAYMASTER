d0ead4fde3b7ddfe08f6fc6ddc46c55aa649d5b3
Triplefonk <like42day@gmail.com>
2023-08-19 14:19:02 +0300

    Init

---
+++ src/New_lesson/Kfs.java
@@ -0,0 +1,11 @@
+package New_lesson;
+
+public class Kfs {
+    public static void main(String[] args) {
+        int k = 0x33E4F;
+        System.out.println(k);
+        System.out.println();
+
+    }
+
+}
